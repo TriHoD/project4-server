68e30f20451c70e9152b3c02a3d251f0a348e127
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2020-01-06 20:41:13 -0800

    Fixed config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://tri_ho:NAv9Ge9NvoRMrMfV@cluster0-tf7qb.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'A SINGLE MEOW'
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./config');
+const { MONGODB } = require('./config.js');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers/')
 
