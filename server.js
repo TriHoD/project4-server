d575423b9fb66903bb567b1327656543ea03829a
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2020-01-06 19:59:02 -0800

    server ready to deploy

---
+++ server.js
@@ -0,0 +1,28 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers/')
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 3313;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req, pubsub })
+});
+
+mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('MongoDB  connected')
+        return server.listen({ port: PORT });
+    })
+    .then((res) => {
+        console.log(`Server is running at ${res.url}`)
+    })
+    .catch(error => {
+        console.log(error)
+    });
